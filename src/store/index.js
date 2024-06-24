d39567a549298ec29fe590a66c9acb866031affc
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-06-24 16:09:00 +0200

    min

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,31 @@
 import { createStore } from 'vuex'
+const productsUrl = 'https://localhost:3000/results';
 
 export default createStore({
   state: {
+    products: null,
+    product: null
   },
   getters: {
   },
   mutations: {
+    setProducts(state, products){
+      state.products = products
+    },
+    setProduct(state, product){
+      state.product = product
+    }
   },
   actions: {
+    async fetchProducts(context){
+      try {
+        let res = await fetch (productsUrl)
+        let data = await res.json()
+        context.commit('setProducts', data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
   },
   modules: {
   }
---
+++ src/views/ProductView.vue
@@ -0,0 +1,15 @@
+<template>
+    <CardComp/>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import CardComp from '@/components/CardComp.vue'
+  
+  export default {
+    name: 'ProductView',
+    components: {
+      CardComp
+    }
+  }
+  </script>
